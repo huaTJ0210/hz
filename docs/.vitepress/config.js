de947a45be62822db3256c496c792a6009da687c
hz <huatj0210@163.com>
2023-11-26 12:30:28 +0800

    feat: docs init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,40 @@
+export default {
+  base: "/docs/",
+  title: "華的文档", //站点标题
+  description: "编程文档集合",
+  themeConfig: {
+    logo: "/logo.png",
+    nav: [
+      // {
+      //   text: "前端",
+      //   link: "/fe/html/index",
+      // },
+    ],
+    sidebar: {
+      "/fe/": [
+        {
+          text: "基础知识",
+          items: [
+            { text: "html", link: "/fe/html/index" },
+            { text: "css", link: "/fe/css/index" },
+            { text: "javascript", link: "/fe/js/index" },
+            { text: "typescript", link: "/fe/ts/index" },
+            {
+              text: "vue",
+              items: [
+                { text: "vue2", link: "/fe/vue/vue2" },
+                { text: "vue3", link: "/fe/vue/vue3" },
+              ],
+            },
+            { text: "vue3", link: "/fe/vue/vue3" },
+            { text: "react", link: "/fe/js/index" },
+          ],
+        },
+      ],
+      "/se/": {
+        text: "基础知识",
+        items: [{ text: "java", link: "/se/java/index" }],
+      },
+    },
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+
+hero:
+  name: 華的编程文档
+  text:
+  tagline: 记录编程点滴
+  actions:
+    - theme: brand
+      text: 前端
+      link: /fe/html/index
+    - theme: alt
+      text: 后端
+      link: /se/java/index
+---
